6963d568234497b08dee8f21e875d979405bfc1f
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-06-18 00:38:26 +0500

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    production: true
+    production: true,
+    // host: 'http://localhost:4000/'
+    host: 'http://146.190.56.37:4000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    host: 'http://localhost:4000/'
+    // host: 'http://localhost:4000/'
+    host: 'http://146.190.56.37:4000/'
 };
