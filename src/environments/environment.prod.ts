f20ce8aaf41c1b16174f2dd67c8ebb5eb692d485
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-06-19 23:27:13 +0500

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // host: 'http://localhost:4000/'
-    host: 'https://api.gmatuzbekistan.uz'
+    host: 'https://api.gmatuzbekistan.uz/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // host: 'http://localhost:4000/'
-    host: 'https://api.gmatuzbekistan.uz'
+    host: 'https://api.gmatuzbekistan.uz/'
 };
